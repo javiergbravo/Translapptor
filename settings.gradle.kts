323af3a5b9575ab36b1e53f828a0463200e3f46a
Javier Gutiérrez Bravo <jgbravo.developer@gmail.com>
2023-01-13 22:34:23 +0100

    feature: setup configuration

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Translapptor"
+include(":androidApp")
+include(":shared")
