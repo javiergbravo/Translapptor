d0e468f448a8346067cefa9f555443875758aa77
Javier Gutiérrez Bravo <jgbravo.developer@gmail.com>
2023-01-15 18:36:03 +0100

    feature - android: setup app theme

---
+++ shared/src/commonMain/kotlin/com/jgbravo/translapptor/core/presentation/Color.kt
@@ -0,0 +1,9 @@
+package com.jgbravo.translapptor.core.presentation
+
+object Colors {
+    const val AccentViolet = 0xFF5643C9
+    const val DarkGrey = 0xFF282C31
+    const val LightBlue = 0xFFA8A5BB
+    const val LightBlueGrey = 0xFFF6F4F4
+    const val TextBlack = 0xFF111111
+}
